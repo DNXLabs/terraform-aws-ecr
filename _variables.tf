09c90ae4bfebe9b4ad97070a2544920ef4db8596
Luiz Rocha <luiz.rocha@brighte.com.au>
2022-02-16 19:18:59 +1100

    Add capability of encrypting ECR repo with CMK

--- _variables.tf
+++ _variables.tf
@@ -6,3 +6,9 @@ variable "trust_accounts" {
   type        = list(string)
   description = "Accounts to trust and allow ECR fetch"
 }
+
+variable "ecr_cmk_encryption" {
+  type        = bool
+  description = "Enabled KMS CMK encryption for ECR repository"
+  default     = false
+}
