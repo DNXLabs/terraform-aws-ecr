2be13ae95fa3e279937bb553696043178179831c
Luiz Rocha <luiz.rocha@brighte.com.au>
2022-03-09 18:04:46 +1100

    Removing deprecated configuration

--- ecr-repositories.tf
+++ ecr-repositories.tf
@@ -1,10 +1,6 @@
 resource "aws_ecr_repository" "default" {
   name = var.name
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
   encryption_configuration {
     encryption_type = "KMS"
     kms_key         = length(var.kms_key_arn) > 0 ? var.kms_key_arn : ""
